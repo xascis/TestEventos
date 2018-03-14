e7b3a381f6f353823d60ce6723e885441296daf7
Francisco <frapagis@hotmail.com>
2018-03-14 11:42:40 +0100

    Añado botón más info

--- platforms/browser/www/funciones.js
+++ platforms/browser/www/funciones.js
@@ -1,23 +1,29 @@
 var wiki="";
+var eventoMinuscula="";
 function muestraEvento(mEvento){
     switch (mEvento){
         case "carnaval":
+        eventoMinuscula="carnaval"
         evento ="Carnaval";
         wiki="El carnaval es una celebración que tiene lugar inmediatamente antes del inicio de la cuaresma cristiana, que se inicia a su vez con el Miércoles de Ceniza, que tiene fecha variable (entre febrero y marzo según el año). El carnaval combina algunos elementos como disfraces, desfiles, y fiestas en la calle.";
         break;
         case "fallas":
+        eventoMinuscula="fallas"
         evento="Fallas";
         wiki = "Las Fallas (Falles en valenciano) son unas fiestas que van del 15 al 19 de marzo con una tradición arraigada en la ciudad de Valencia y diferentes poblaciones de la Comunidad Valenciana. Oficialmente empiezan el último domingo de febrero con el acto de la Crida.";
         break;
         case "nochevieja":
+        eventoMinuscula="nochevieja"
         evento="Nochevieja";
         wiki = "La Nochevieja, víspera de Año Nuevo, Año Viejo o fin de año, es la última noche del año en el calendario gregoriano, comprendiendo desde 31 de diciembre hasta el 1 de enero (Año Nuevo). Desde que se cambió al calendario gregoriano en el año 1582, se suele celebrar esta festividad, aunque ha ido evolucionando en sus costumbres y supersticiones.";
         break;
         case "sanjuan":
+        eventoMinuscula="sanjuan"
         evento="Noche de San Juan";
         wiki ="La víspera de San Juan o noche de San Juan es una festividad cristiana, de origen pagano (Litha) celebrada el 23 de junio,1 en la que se suelen encender hogueras o fuegos y ligada con las celebraciones en las que se festejaba la llegada del solsticio de verano, el 21 de junio en el hemisferio norte, cuyo rito principal consiste en encender una hoguera.";
         break;
         case "semanasanta":
+        eventoMinuscula="semanasanta"
         evento="Semana Santa";
         wiki="La Semana Santa es la conmemoración anual cristiana de la Pasión, Muerte y Resurrección de Jesús de Nazaret. Por eso, es un período de intensa actividad litúrgica dentro de las diversas confesiones cristianas. Da comienzo el Domingo de Ramos y finaliza el Domingo de Resurrección,1 aunque su celebración suele iniciarse en varios lugares el viernes anterior (Viernes de Dolores) y se considera parte de la misma el Domingo de Resurrección.";
         break;
@@ -29,7 +35,17 @@ function muestraEvento(mEvento){
 function muestra(mEvento, mWiki){
     document.getElementById("evento").innerHTML=mEvento;
     document.getElementById("wiki").innerHTML=mWiki;
+    document.getElementById('infoButton').onclick = function(){ cargarContenidoURL('https://us-central1-eventos-98475.cloudfunctions.net/mostrarEventosHtml?evento='); } ;
 }
 function volver(){
     jsInterfazNativa.volver();
+}
+function cargarContenidoURL(urlPage){
+    $.ajax({
+        url: urlPage + eventoMinuscula,
+        cache: false
+    }).done(function(data){
+        $("#wiki").html(data);
+        document.getElementById('infoButton').onclick = function(){ muestraEvento(eventoMinuscula) } ;
+    })
 }
